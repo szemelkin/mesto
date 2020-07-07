a870e44d883288f1ddd352f01751ad7ae44c0fd0
Sergey Zemelkin <zemelkins@yandex.ru>
2020-07-08 00:24:50 +0300

    Верстка 080720 в1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const openModalButton = document.querySelector('.profile__edit_button');
+const openModalButton = document.querySelector('.profile__edit-button');
 const closeModalButton = document.querySelector('.modal__close-button');
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.modal__form');
@@ -13,28 +13,28 @@ const profileText = document.querySelector('.profile__text');
 
 
 function toggleModal() {
+
+  if (!modal.classList.contains('modal_is-open')) {
+    inputName.value = profileName.textContent;
+    inputStatus.value = profileText.textContent;
+  }
+
   modal.classList.toggle('modal_is-open')
 }
 
 openModalButton.addEventListener('click', toggleModal);
 
 
-closeModalButton.addEventListener('click', (e) => {
-  inputName.value = profileName.textContent;
-  inputStatus.value = profileText.textContent;
-  toggleModal()
-  e.preventDefault();
-});
+closeModalButton.addEventListener('click', toggleModal);
 
 
 form.addEventListener('submit', (e) => {
+  e.preventDefault();
 
   profileName.textContent = inputName.value;
   profileText.textContent = inputStatus.value;
 
   toggleModal();
-
-  e.preventDefault();
 
 })
 
