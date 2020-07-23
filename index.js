1c3ed9f69ad182e23a6c550b07ddb8aea5259f96
Sergey Zemelkin <zemelkins@yandex.ru>
2020-07-23 19:24:54 +0300

    Верстка 23072020 в2

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
+//Конпки для едкатирования прфиля
 const openModalButton = document.querySelector('.profile__edit-button');
-const closeModalButton = document.querySelector('.modal__close-button');
+//const closeModalButton = document.querySelector('.modal__close-button');
+const openAddCardButton = document.querySelector('.profile__add-button')
+
+//Модальная форма для редактирования профиля//
 const modal = document.querySelector('.modal');
 const form = document.querySelector('.modal__form');
-
 const inputName = document.querySelector('.modal__input_type_name');
 const inputStatus = document.querySelector('.modal__input_type_status');
-
 const profileName = document.querySelector('.profile__title');
 const profileText = document.querySelector('.profile__text');
 
+//Карточки по умолчанию//
 const initialCards = [
   {
       name: 'Архыз',
@@ -36,24 +39,36 @@ const initialCards = [
   }
 ];
 
+//Вставляем карточки
 const cardsListElement = document.querySelector('.elements');
 const elementTemplate = document.querySelector('.element-template');
+const card = elementTemplate.content.cloneNode(true);
+const cardLikeButton = card.querySelector('.element__heart');
+const cardDeleteButton = card.querySelector('.element__delete');
 
 
-function addCard(link, name) {
+//const modalTemplate = document.querySelector('.modal-template');
+
+
+
+
+function addCard(data) {
   const card = elementTemplate.content.cloneNode(true);
-  card.querySelector('img').src = link;
-  card.querySelector('.element__title').textContent = name;
+  const cardLikeButton = card.querySelector('.element__heart');
+  const cardDeleteButton = card.querySelector('.element__delete');
+
+  card.querySelector('img').src = data.link;
+  card.querySelector('.element__title').textContent = data.name;
   cardsListElement.prepend(card)
 
 }
 
-initialCards.forEach(card => {
-  addCard(card.link, card.name);
+initialCards.forEach(data => {
+  addCard(data);
 })
 
 
-
+//Добавление формы при работе с профилем
 function toggleModal() {
 
   if (!modal.classList.contains('modal_is-open')) {
@@ -63,11 +78,6 @@ function toggleModal() {
 
   modal.classList.toggle('modal_is-open');
 }
-
-openModalButton.addEventListener('click', toggleModal);
-
-
-closeModalButton.addEventListener('click', toggleModal);
 
 
 form.addEventListener('submit', (e) => {
@@ -79,6 +89,68 @@ form.addEventListener('submit', (e) => {
   toggleModal();
 
 })
+
+
+openModalButton.addEventListener('click', toggleModal);
+
+//closeModalButton.addEventListener('click', toggleModal);
+
+const openFormAddCard = document.querySelector('.profile__add-button');
+
+openFormAddCard.addEventListener('click', addModalForm);
+
+
+const closeModalButton = document.querySelector('.modal__close-button');
+closeModalButton.addEventListener('click', closeModalForm);
+
+
+
+
+
+
+
+
+//Добавлене карточек с помощью шаблона
+
+//openAddCardButton.addEventListener('click', toggleAddCardModal);
+
+//Поучаем элемент внутри тега темплейта формы
+const templateModalForm = document.querySelector('.modal-template');
+//Клонируем
+const templateModalFormClone = templateModalForm.cloneNode(true);
+
+
+//Получаем элемент внутри темплейта
+const templateModalContent = templateModalFormClone.content;
+
+//Находим нужный класс
+const templateModalSection = templateModalContent.querySelector('.modal');
+
+//Добавляем форму из темплейта
+//templateModalSection.classList.add('modal_is-open');
+
+//Определяем относительного какого ДОМ будем вставлять
+const elementBody= document.querySelector('body');
+
+//Добавляем в темплейт ДОМ
+
+//elementBody.append(templateModalContent);
+
+
+function addModalForm() {
+  templateModalSection.classList.toggle('modal_is-open');
+  elementBody.append(templateModalContent);
+
+}
+
+
+function closeModalForm(e) {
+  const templateModalContent = e.target.closest('.modal');
+  console.log(templateModalContent);
+  templateModalContent.remove();
+}
+
+
 
 
 
