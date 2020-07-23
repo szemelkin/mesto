e2eb2555e20ad3bedf05d3fc84dded95c9c7d26c
Sergey Zemelkin <zemelkins@yandex.ru>
2020-07-23 06:22:01 +0300

    Верстка 23072020 в1

--- index.js
+++ index.js
@@ -9,6 +9,48 @@ const inputStatus = document.querySelector('.modal__input_type_status');
 const profileName = document.querySelector('.profile__title');
 const profileText = document.querySelector('.profile__text');
 
+const initialCards = [
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+const cardsListElement = document.querySelector('.elements');
+const elementTemplate = document.querySelector('.element-template');
+
+
+function addCard(link, name) {
+  const card = elementTemplate.content.cloneNode(true);
+  card.querySelector('img').src = link;
+  card.querySelector('.element__title').textContent = name;
+  cardsListElement.prepend(card)
+
+}
+
+initialCards.forEach(card => {
+  addCard(card.link, card.name);
+})
 
 
 
