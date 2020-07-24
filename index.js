f1edb07e499b167dbc6a5b5486e7ffd06a1c0679
Sergey Zemelkin <zemelkins@yandex.ru>
2020-07-24 07:43:01 +0300

    Верстка 24072020 в1

--- index.js
+++ index.js
@@ -1,15 +1,84 @@
-//Конпки для едкатирования прфиля
-const openModalButton = document.querySelector('.profile__edit-button');
-//const closeModalButton = document.querySelector('.modal__close-button');
-const openAddCardButton = document.querySelector('.profile__add-button')
+//Открытие модалок
+const openAddCardModalButton = document.querySelector(".profile__add-button");
+const openEditProfileModalButton = document.querySelector('.profile__edit-button');
 
-//Модальная форма для редактирования профиля//
-const modal = document.querySelector('.modal');
-const form = document.querySelector('.modal__form');
+//Идентификация типа модалки
+const addCardModal = document.querySelector('.modal_type_add-card')
+const editProfileModal = document.querySelector('.modal_type_edit-profile');
+
+//Поиск формы в модалке
+const formAddCard = addCardModal.querySelector('.modal__form');
+const formEditProfileModel = editProfileModal.querySelector('.modal__form');
+
+//Поля для ввода
 const inputName = document.querySelector('.modal__input_type_name');
 const inputStatus = document.querySelector('.modal__input_type_status');
+
+//Поля для вывода
 const profileName = document.querySelector('.profile__title');
 const profileText = document.querySelector('.profile__text');
+
+//Поиск кнопки закрытия в модалке
+const addCardCloseModalButton = addCardModal.querySelector('.modal__close-button');
+const editProfileCloseModalButton = editProfileModal.querySelector('.modal__close-button');
+
+
+
+//Функция открытия-закрытия модалки
+function toggleModal(modal) {
+
+  if (!modal.classList.contains('modal_is-open')) {
+    inputName.value = profileName.textContent;
+    inputStatus.value = profileText.textContent;
+  }
+
+  modal.classList.toggle('modal_is-open');
+}
+
+//Функция сохранения модалки при редактировании профиля
+function profileEditHandler(e) {
+  e.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileText.textContent = inputStatus.value;
+
+  toggleModal(editProfileModal);
+};
+
+//Функция сохранения модалки при добавлении карточки
+function profileAddCardHandler(e) {
+  e.preventDefault();
+
+
+
+  toggleModal(addCardModal);
+};
+
+
+
+//Открытие и закрытие модалки с добавлением карточки
+openAddCardModalButton.addEventListener('click', () => {
+  toggleModal(addCardModal)
+});
+addCardCloseModalButton.addEventListener('click', () => {
+  toggleModal(addCardModal)
+});
+
+//Открытие и закрытие модалки с редактированием профиля
+openEditProfileModalButton.addEventListener('click', () => {
+  toggleModal(editProfileModal)
+});
+editProfileCloseModalButton.addEventListener('click', () => {
+  toggleModal(editProfileModal)
+});
+
+//Обработчики сохранения модалок
+formEditProfileModel.addEventListener('submit', profileEditHandler)
+formAddCard.addEventListener('submit', profileAddCardHandler)
+
+
+
+
 
 //Карточки по умолчанию//
 const initialCards = [
@@ -39,118 +108,37 @@ const initialCards = [
   }
 ];
 
-//Вставляем карточки
+//Переменные для вставки карточек
 const cardsListElement = document.querySelector('.elements');
 const elementTemplate = document.querySelector('.element-template');
 const card = elementTemplate.content.cloneNode(true);
 const cardLikeButton = card.querySelector('.element__heart');
 const cardDeleteButton = card.querySelector('.element__delete');
 
-
-//const modalTemplate = document.querySelector('.modal-template');
-
-
-
-
+//Функция для добавления карточек
 function addCard(data) {
   const card = elementTemplate.content.cloneNode(true);
-  const cardLikeButton = card.querySelector('.element__heart');
-  const cardDeleteButton = card.querySelector('.element__delete');
 
   card.querySelector('img').src = data.link;
   card.querySelector('.element__title').textContent = data.name;
   cardsListElement.prepend(card)
-
 }
 
+//Функция для пробега по масиву первоначальных карточек
 initialCards.forEach(data => {
   addCard(data);
 })
 
-
-//Добавление формы при работе с профилем
-function toggleModal() {
-
-  if (!modal.classList.contains('modal_is-open')) {
-    inputName.value = profileName.textContent;
-    inputStatus.value = profileText.textContent;
-  }
-
-  modal.classList.toggle('modal_is-open');
-}
-
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-
-  profileName.textContent = inputName.value;
-  profileText.textContent = inputStatus.value;
-
-  toggleModal();
-
+//Кнопки на карточках
+cardLikeButton.addEventListener('click', () => {
+  //handleLikeClick()
 })
 
+cardDeleteButton.addEventListener('click', () => {
+  //handleDeleteClick()
+})
 
-openModalButton.addEventListener('click', toggleModal);
-
-//closeModalButton.addEventListener('click', toggleModal);
-
-const openFormAddCard = document.querySelector('.profile__add-button');
-
-openFormAddCard.addEventListener('click', addModalForm);
-
-
-const closeModalButton = document.querySelector('.modal__close-button');
-closeModalButton.addEventListener('click', closeModalForm);
-
-
-
-
-
-
-
-
-//Добавлене карточек с помощью шаблона
-
-//openAddCardButton.addEventListener('click', toggleAddCardModal);
-
-//Поучаем элемент внутри тега темплейта формы
-const templateModalForm = document.querySelector('.modal-template');
-//Клонируем
-const templateModalFormClone = templateModalForm.cloneNode(true);
-
-
-//Получаем элемент внутри темплейта
-const templateModalContent = templateModalFormClone.content;
-
-//Находим нужный класс
-const templateModalSection = templateModalContent.querySelector('.modal');
-
-//Добавляем форму из темплейта
-//templateModalSection.classList.add('modal_is-open');
-
-//Определяем относительного какого ДОМ будем вставлять
-const elementBody= document.querySelector('body');
-
-//Добавляем в темплейт ДОМ
-
-//elementBody.append(templateModalContent);
-
-
-function addModalForm() {
-  templateModalSection.classList.toggle('modal_is-open');
-  elementBody.append(templateModalContent);
-
-}
-
-
-function closeModalForm(e) {
-  const templateModalContent = e.target.closest('.modal');
-  console.log(templateModalContent);
-  templateModalContent.remove();
-}
-
-
-
-
+//cardImage.addEventListener('click', () => {
+  //handleImageClick()
+//})
 
